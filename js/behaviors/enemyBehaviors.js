4247a89926b38a61953a96c24c6fda9166b2d1a6
Emily <eflauto@student.cscc.edu>
2025-11-05 11:39:46 -0500

    Reimplement enemy drone behavior
    Reimplemented the enemy drone behavior which was previously the seeker. This uses a behavior now.

--- js/behaviors/enemyBehaviors.js
+++ js/behaviors/enemyBehaviors.js
@@ -9,14 +9,51 @@ SineBehavior.prototype = {
     },
 
     moveSpriteHorizontally: function (sprite, now, lastAnimationFrameTime) {
+        const SCREEN_WIDTH = 800;
         let pixelsToMoveY = (now - lastAnimationFrameTime) / 1000;
 
-        if ((sprite.top <= 0 && !this.reversed) || (sprite.top + sprite.height >= 400 && this.reversed)) {
+        if ((sprite.top <= 0 && !this.reversed) || (sprite.top + sprite.height >= SCREEN_WIDTH && this.reversed)) {
             this.velocityY = -this.velocityY;
             this.reversed = !this.reversed;
         }
 
         pixelsToMoveY *= this.velocityY;
+        sprite.top += pixelsToMoveY;
+    }
+};
+
+DroneBehavior = function (interval, speed) {
+    this.interval = interval || 1000;
+    this.speed = speed || 60;
+    this.dashing = true;
+    this.lastCycle = 0;
+};
+
+DroneBehavior.prototype = {
+    execute: function (sprite, now, fps, context, lastAnimationFrameTime) {
+        // Implement a cycle based system to toggle between dashing and not. Makes it easier to have dash intervals.
+        if (now - this.lastCycle > this.interval) {
+            this.lastCycle = now;
+            this.dashing = !this.dashing;
+        }
+
+        this.dash(sprite, now, lastAnimationFrameTime);
+    },
+
+    dash: function (sprite, now, lastAnimationFrameTime) {
+        if (!this.dashing) return;
+
+        let pixelsToMoveX = (now - lastAnimationFrameTime) / 1000;
+        let pixelsToMoveY = (now - lastAnimationFrameTime) / 1000;
+        let xDistance = spaceshipGame.player.left - sprite.left;
+        let yDistance = spaceshipGame.player.top - sprite.top;
+        let distance = Math.hypot(xDistance, yDistance);  // Used for normalization
+        let xDirection = xDistance / distance;  // Normalizes the distance to get a direction
+        let yDirection = yDistance / distance;  // Same here
+
+        pixelsToMoveX *= xDirection * this.speed;
+        pixelsToMoveY *= yDirection * this.speed;
+        sprite.left += pixelsToMoveX;
         sprite.top += pixelsToMoveY;
     }
 };
--- js/enemies.js
+++ js/enemies.js
@@ -1,7 +1,7 @@
 function createEnemies(game) {
     // Defines initial spawn points and enemy types
     game.enemyData = [
-        {left: 400, top: 100, type: "wander"},
+        {left: 400, top: 100, type: "drone"},
         {left: 500, top: 160, type: "sine"},
         {left: 600, top: 220, type: "seeker"},
         {left: 700, top: 260, type: "dasher"}
@@ -14,6 +14,9 @@ function createEnemies(game) {
         let enemy = createSprite('tear_drop_drone', 270, [new BulletFiring(2000)]);
 
         switch (data.type) {
+            case "drone":
+                enemy.behaviors.push(new DroneBehavior(500, 80));
+                break;
             case "sine":
                 enemy.behaviors.push(new SineBehavior(-100));
                 break;

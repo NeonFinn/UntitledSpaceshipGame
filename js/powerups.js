38676fb9c85c3fe40ad8e8a94d1fa5343f9f1119
Kyiranes <evilmcchip@gmail.com>
2025-11-27 21:06:16 -0500

    test

--- js/powerups.js
+++ js/powerups.js
@@ -7,7 +7,7 @@ function createPowerUps(game) {
         { left: 300, top: 200, color: 'green' },
         { left: 600, top: 150, color: 'yellow' }
     ];
-
+    powerUpColors = ['red','blue','green','yellow']
     for (let i = 0; i < game.powerUpData.length; i++) {
         let powerUp = createSprite(`${game.powerUpData[i].color}_power_up`, 0, [new CycleBehavior(game.POWER_UP_CYCLE_DURATION, 0)])
 
@@ -21,4 +21,20 @@ function createPowerUps(game) {
         game.powerUps.push(powerUp);
         game.sprites.push(powerUp);
     }
+    pushNewPowerUps = function(){
+        let powerUp = createSprite(`${powerUpColors[Math.floor(Math.random * 4)]}_power_up`, 0, [new CycleBehavior(game.POWER_UP_CYCLE_DURATION, 0)])
+
+        powerUp.velocityX = 25;
+        
+        powerUp.width = 16;
+        powerUp.height = 16;
+        powerUp.collider = true;
+        powerUpX = Math.floor(powerUp.width + screen.width);
+        powerUpY = Math.floor(Math.random() * 100) + 1;
+        powerUp.left = powerUpX;
+        powerUp.top = powerUpY;
+        game.powerUps.push(powerUp);
+        game.sprites.push(powerUp);
+        }
+    setInterval(pushNewPowerUps, 1000)
 }
